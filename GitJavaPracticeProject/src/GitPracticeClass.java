ddb7f20af3a26376c74c91d077b5817a166ce4b0
CodeinHub <codeinhub@gmail.com>
2020-02-13 17:25:04 -0500

    commit

--- GitJavaPracticeProject/src/GitPracticeClass.java
+++ GitJavaPracticeProject/src/GitPracticeClass.java
@@ -4,6 +4,7 @@ public class GitPracticeClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Practice 1");
+		System.out.println("Clone Practice");
 	}
 
 }
